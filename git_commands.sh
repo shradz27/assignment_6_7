e3d4e7ad0a655e0060637daebfdb91b730a7b173
Shradha Luintel <luintel.shradha@gmail.com>
2019-12-05 21:10:08 -0500

    Add git command bash script

---
+++ git_commands.sh
@@ -0,0 +1,8 @@
+# from the root directory of the files
+git init # initialize the repo
+git add .
+# create gitignore file
+git commit -m "first commit"
+git remote add origin https://github.com/shradz27/assignment_7.git
+git push -u origin master
+
